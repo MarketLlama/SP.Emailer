6fe582a0537fffd8615d0e936b987a7d9a0eab50
Williams Mark (ext) GBMC <s951754@syngenta.org>
2018-12-17 17:15:08 +0000

    Bug fixes and styling

--- src/webparts/sharePointEmailer/components/SharePointEmailer.tsx
+++ src/webparts/sharePointEmailer/components/SharePointEmailer.tsx
@@ -4,7 +4,7 @@ import { ISharePointEmailerProps } from './ISharePointEmailerProps';
 import { ISharePointEmailerState } from './ISharePointEmailerState';
 import { escape } from '@microsoft/sp-lodash-subset';
 import { Modal } from 'office-ui-fabric-react/lib/Modal';
-import { PrimaryButton ,ActionButton } from 'office-ui-fabric-react/lib/Button';
+import { PrimaryButton, ActionButton } from 'office-ui-fabric-react/lib/Button';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
 import { sp, EmailProperties, Web } from "@pnp/sp";
 import { PeoplePicker, PrincipalType } from "@pnp/spfx-controls-react/lib/PeoplePicker";
@@ -12,14 +12,19 @@ import '../emailTemplates/standardEmailTemplate.html';
 
 export default class SharePointEmailer extends React.Component<ISharePointEmailerProps, ISharePointEmailerState> {
   private _emailTemplate = require("../emailTemplates/standardEmailTemplate.html");
+  private _users = [];
   private _toUsers = [];
   private _defaultUsers = [];
+  private _currentPage = {
+    Title: '',
+    FileRef: ''
+  };
 
   constructor(props) {
     super(props);
     this.state = {
       showModal: false,
-      emailText : ''
+      emailText: ''
     };
 
     this._main = this._main.bind(this);
@@ -29,8 +34,8 @@ export default class SharePointEmailer extends React.Component<ISharePointEmaile
 
   public render(): React.ReactElement<ISharePointEmailerProps> {
     return (
-      <div className={ styles.sharePointEmailer }>
-        <div className={ styles.container }>
+      <div className={styles.sharePointEmailer}>
+        <div className={styles.container}>
           <ActionButton iconProps={{ iconName: 'Mail' }} secondaryText="Opens the Sample Modal" onClick={this._showModal} text="Send Email" />
           <Modal
             titleAriaId="titleId"
@@ -38,11 +43,11 @@ export default class SharePointEmailer extends React.Component<ISharePointEmaile
             isOpen={this.state.showModal}
             onDismiss={this._closeModal}
             isBlocking={false}
-            className={ styles.modalContainer }
+            className={styles.modalContainer}
           >
             <div className={styles.modalHeader}>
-              <span style={{padding : "20px"}} id="titleId">Send Email</span>
-              <ActionButton className={styles.closeButton} iconProps={{ iconName: 'Cancel' }} onClick={this._closeModal}/>
+              <span style={{ padding: "20px" }} id="titleId">Send Email</span>
+              <ActionButton className={styles.closeButton} iconProps={{ iconName: 'Cancel' }} onClick={this._closeModal} />
             </div>
             <div id="subtitleId" className={styles.modalBody}>
               <PeoplePicker
@@ -51,7 +56,6 @@ export default class SharePointEmailer extends React.Component<ISharePointEmaile
                 groupName={""} // Leave this blank in case you want to filter from all users
                 showtooltip={true}
                 isRequired={true}
-                disabled={false}
                 defaultSelectedUsers={this._defaultUsers}
                 selectedItems={this._getPeoplePickerItems}
                 showHiddenInUI={false}
@@ -59,14 +63,14 @@ export default class SharePointEmailer extends React.Component<ISharePointEmaile
               <p>
                 Enter content of email.{' '}
               </p>
-              <TextField multiline rows={11} onChanged={this._setMailText} value={this.state.emailText}/>
-              <br/>
+              <TextField multiline rows={11} onChanged={this._setMailText} value={this.state.emailText} />
+              <br />
               <PrimaryButton
-                  iconProps={{ iconName: 'Mail' }}
-                  text="Send Mail"
-                  onClick={this._sendEmail}
-                  style={{float:"right"}}
-                />
+                iconProps={{ iconName: 'Mail' }}
+                text="Send Mail"
+                onClick={this._sendEmail}
+                style={{ float: "right" }}
+              />
             </div>
           </Modal>
         </div>
@@ -85,25 +89,27 @@ export default class SharePointEmailer extends React.Component<ISharePointEmaile
 
   private _setMailText = (newText: string): void => {
     this.setState({
-      emailText : newText,
+      emailText: newText,
     });
   }
 
   private _getPeoplePickerItems(items: any[]) {
-    items.forEach(item =>{
+    items.forEach(item => {
       this._toUsers.push(item.secondaryText);
     });
   }
 
   private _getSubscriptions() {
     return new Promise((resolve, reject) => {
-      const web = new Web(this.props.context.pageContext.site.absoluteUrl);
-
       try {
-        const PageID = 24;//this.props.context.pageContext.listItem.id;
-        web.lists.getByTitle("Subscriptions").items.filter("PageID eq '" + PageID + "'").get().then((items: any[]) => {
+        const PageID = this.props.context.pageContext.listItem.id;
+
+        const parentWeb = new Web(this.props.context.pageContext.site.absoluteUrl);
+
+        parentWeb.lists.getByTitle("Subscriptions").items.filter("PageID eq '" + PageID + "'").get().then((items: any[]) => {
           resolve(items);
         });
+
       } catch (e) {
         console.log(e);
         reject();
@@ -112,46 +118,58 @@ export default class SharePointEmailer extends React.Component<ISharePointEmaile
     });
   }
 
-  private _getEmailContent = () : string =>{
+  private _getPageDetails = async () => {
+    var id = this.props.context.pageContext.listItem.id;
+    const pages = sp.web.lists.getByTitle('Pages').items;
+    let page = await pages.getById(id).select("Title", "FileRef").get();
+    return page;
+  }
+
+  private _getEmailContent = (user: any): string => {
     let emailTemplate = this._emailTemplate.toString();
-    emailTemplate.replace('{{pageContent}}', this.state.emailText);
-    emailTemplate.replace('{{pageURL}}',this.props.context.pageContext.web.absoluteUrl);
-    emailTemplate.replace('{{userName}}','Some Name');
-    emailTemplate.replace('{{pageTitle}}', 'Some Title');
-    console.log(emailTemplate);
-    //this._emailTemplate = emailTemplate;
+    let tenantUri = window.location.protocol + "//" + window.location.host;
+    emailTemplate = emailTemplate.replace(/{{emailContent}}/gi, this.state.emailText)
+      .replace(/{{pageURL}}/gi, tenantUri + this._currentPage.FileRef)
+      .replace(/{{userName}}/gi, user.UserName)
+      .replace(/{{pageTitle}}/gi, this._currentPage.Title);
 
     return emailTemplate;
   }
 
-  private _sendEmail = () : void =>{
+  private _sendEmail = (): void => {
 
-      let emailContent = this._getEmailContent();
+    this._users.forEach(async user => {
+      let emailContent = this._getEmailContent(user);
 
       const emailProps: EmailProperties = {
-          To: this._toUsers,
-          Subject: "This email is about...",
-          Body: emailContent,
+        To: [user.Title],
+        Subject: "Syngenta Positions : " + this._currentPage.Title,
+        Body: emailContent
       };
 
-      sp.utility.sendEmail(emailProps).then(_ => {
-          console.log("Email Sent!");
-      });
+      await sp.utility.sendEmail(emailProps);
+    });
+
   }
 
-  public componentDidMount (){
+  public componentDidMount() {
     this._main();
   }
 
-  private async _main(){
+  private async _main() {
+    //Get details of current page.
+    this._currentPage = await this._getPageDetails();
+    console.log(this._currentPage);
     //Gets the subsribers of the page and sets them as default email contacts for the emailer.
     //Uses the defaultSelectedUsers 
     let subscriptions = await this._getSubscriptions();
-    console.log(subscriptions);
     let string = JSON.stringify(subscriptions);
-    let arr : any[] = JSON.parse(string);
-    arr.forEach(item=>{
+    let arr: any[] = JSON.parse(string);
+    this._users = arr;
+    arr.forEach(item => {
       this._defaultUsers.push(item.Title);
+      this._toUsers.push(item.Title);
     });
+
   }
 }
