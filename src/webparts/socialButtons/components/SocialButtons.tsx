ef89ecc1726bba3eea2d6634b2e3cf9ea5b6221f
Mark Williams <mark@Marks-MBP.home>
2019-05-27 02:00:24 +0100

    Added List instance

--- src/webparts/socialButtons/components/SocialButtons.tsx
+++ src/webparts/socialButtons/components/SocialButtons.tsx
@@ -2,24 +2,70 @@ import * as React from 'react';
 import styles from './SocialButtons.module.scss';
 import { ISocialButtonsProps } from './ISocialButtonsProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { Button } from 'office-ui-fabric-react';
 
-export default class SocialButtons extends React.Component<ISocialButtonsProps, {}> {
+export interface ISocialButtonsState {
+  isSubscribed : boolean;
+}
+
+
+export default class SocialButtons extends React.Component<ISocialButtonsProps, ISocialButtonsState> {
+  constructor(props : ISocialButtonsProps){
+    super(props);
+    this.state ={
+      isSubscribed : true //To do with prop
+    };
+  }
   public render(): React.ReactElement<ISocialButtonsProps> {
     return (
       <div className={ styles.socialButtons }>
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
+              {!this.state.isSubscribed?
+              <Button className={styles.button}
+                iconProps={{iconName : "Heart"}}
+                text="Subscribe"
+                onClick={this._subscribeToPage}
+              /> :
+              <Button className={styles.button}
+                iconProps={{iconName : "HeartBroken"}}
+                text="Unsubscribe"
+                onClick={this._unsubscribeToPage}
+              />}
+            </div>
+            <div className={ styles.column }>
+              <Button className={styles.yammerButton}
+                iconProps={{iconName : "YammerLogo"}}
+                text="Share"
+                onClick={this._sharePageOnYammer}
+              />
             </div>
           </div>
         </div>
       </div>
     );
   }
+
+  private _subscribeToPage = () =>{
+    console.log('Subscribed');
+    this.setState({
+      isSubscribed : true
+    });
+  }
+
+  private _unsubscribeToPage =() =>{
+    this.setState({
+      isSubscribed : false
+    });
+  }
+
+  private _sharePageOnYammer = ()=>{
+    console.log('Shared');
+  }
+
+  private _isSubscribedToPage = ()=>{
+
+  }
+
 }
