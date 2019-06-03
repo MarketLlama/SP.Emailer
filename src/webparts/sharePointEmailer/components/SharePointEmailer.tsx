1bd1323ac76c8b7e7df48c74186a7cf02516cf83
Mark Williams <mark@Marks-MacBook-Pro.local>
2019-06-03 16:46:00 +0100

    Better feedback when subscribing

--- src/webparts/sharePointEmailer/components/SharePointEmailer.tsx
+++ src/webparts/sharePointEmailer/components/SharePointEmailer.tsx
@@ -112,7 +112,6 @@ export default class SharePointEmailer extends React.Component<ISharePointEmaile
         console.log(e);
         reject();
       }
-
     });
   }
 
--- src/webparts/socialButtons/components/SocialButtons.tsx
+++ src/webparts/socialButtons/components/SocialButtons.tsx
@@ -4,6 +4,7 @@ import { ISocialButtonsProps } from './ISocialButtonsProps';
 import { Icon } from 'office-ui-fabric-react';
 import { sp, Items, ItemAddResult } from "@pnp/sp";
 import ReactTooltip from 'react-tooltip';
+import swal from 'sweetalert';
 
 declare var yam;
 
@@ -36,20 +37,24 @@ export default class SocialButtons extends React.Component<ISocialButtonsProps, 
         <div className={ styles.container }>
           <ul className={styles.socialButtonList} >
             <li>
-            {!this.state.isSubscribed?
-              <a data-tip data-for='subscribe' onClick={this._subscribeToPage} className={styles.subscribeButton}>
-                <Icon iconName="Heart" />
-              </a> :
-              <a data-tip data-for='unsubscribe' onClick={this._unsubscribeToPage} className={styles.subscribeButton}>
-                <Icon iconName="HeartBroken" />
-              </a>
+            {this.state.isSubscribed?
+              <div>
+                <a data-tip data-for='unsubscribe' onClick={this._unsubscribeToPage} className={styles.subscribeButton}>
+                  <Icon iconName="HeartBroken" />
+                </a>
+                <ReactTooltip id='unsubscribe' type='dark' effect='solid'>
+                  <span>Unsubscribe to article</span>
+                </ReactTooltip>
+              </div> :
+              <div>
+                <a data-tip data-for='subscribe' onClick={this._subscribeToPage} className={styles.subscribeButton}>
+                  <Icon iconName="Heart" />
+                </a>
+                <ReactTooltip id='subscribe' type='dark' effect='solid'>
+                  <span>Subscribe to article</span>
+                </ReactTooltip>
+              </div>
             }
-              <ReactTooltip id='subscribe' type='dark' effect='solid'>
-                <span>Subscribe to article</span>
-              </ReactTooltip>
-              <ReactTooltip id='unsubscribe' type='dark' effect='solid'>
-                <span>Unsubscribe to article</span>
-              </ReactTooltip>
             </li>
             <li>
               <a data-tip data-for='yammer' href="#" className={`${styles.yammerButton} yammer-button`}>
@@ -82,6 +87,20 @@ export default class SocialButtons extends React.Component<ISocialButtonsProps, 
       this.setState({
         isSubscribed : true
       });
+      swal({
+        title: "Subscribed!",
+        text: `You are subscribed to this page to recieve email updates.`,
+        icon: "success",
+        buttons : {
+            confirm: {
+              text: "OK",
+              value: true,
+              visible: true,
+              className: "",
+              closeModal: true
+            }
+        }
+      });
     }, err =>{
       console.log(err);
     });
@@ -92,10 +111,24 @@ export default class SocialButtons extends React.Component<ISocialButtonsProps, 
     const UserID = this.props.context.pageContext.user.loginName;
     this._subScriptionListItems.filter(`SubscriptionPageID eq '${PageID}' and SubscriptionUserID eq '${UserID}'`)
       .get().then((value : any[]) =>{
-        if(value){
+        if(value.length > 0 ){
           this._subScriptionListItems.getById(value[0].Id).delete().then(v =>{
             this.setState({
               isSubscribed : false
+            });
+            swal({
+              title: "Unsubscribed!",
+              text: `You have unsubscribed from this article.`,
+              icon: "info",
+              buttons : {
+                  confirm: {
+                    text: "OK",
+                    value: true,
+                    visible: true,
+                    className: "",
+                    closeModal: true
+                  }
+              }
             });
           }, err => console.log(err));
         }
@@ -105,13 +138,12 @@ export default class SocialButtons extends React.Component<ISocialButtonsProps, 
 
   }
 
-
   private _isSubscribedToPage = ()=>{
     const PageID = this.props.pageId;
     const UserID = this.props.context.pageContext.user.loginName;
     this._subScriptionListItems.filter(`SubscriptionPageID eq '${PageID}' and SubscriptionUserID eq '${UserID}'`)
       .get().then((value : any[]) =>{
-        if(value){
+        if(value.length > 0){
           this.setState({
             isSubscribed: true
           });
